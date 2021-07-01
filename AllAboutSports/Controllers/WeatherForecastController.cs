cb58367e67270c96ecbd92848088b0934d6c0e0c
IordachitaFlorinD <86802623+IordachitaFlorinD@users.noreply.github.com>
2021-07-02 00:09:00 +0300

    adding value to string readonly in weather forecast controller

--- AllAboutSports/Controllers/WeatherForecastController.cs
+++ AllAboutSports/Controllers/WeatherForecastController.cs
@@ -14,7 +14,7 @@ namespace AllAboutSports.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Florin"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
